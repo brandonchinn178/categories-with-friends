76082b304086c4830f01fd8b3909c0e8c73d4689
Brandon Chinn <brandon@leapyear.io>
2020-03-29 01:00:37 -0700

    Add categories list

--- server/src/Scattergories/Game.hs
+++ server/src/Scattergories/Game.hs
@@ -1,22 +1,30 @@
 {-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE TemplateHaskell #-}
 
 module Scattergories.Game
   ( Game
   , PlayerName
   , GameStatus(..)
+  , RoundInfo(..)
   , RoundStatus(..)
   , createGame
   , getStatus
   , getHost
   , initPlayer
   , getPlayers
+  , generateRound
   , Category
   , Answer
   ) where
 
+import Control.Monad.Random (getRandomR)
+import Data.FileEmbed (embedStringFile)
 import Data.Set (Set)
 import qualified Data.Set as Set
 import Data.Text (Text)
+import qualified Data.Text as Text
+import System.Random.Shuffle (shuffleM)
 
 {- Game infrastructure -}
 
@@ -31,9 +39,15 @@ data Game = Game
 
 data GameStatus
   = GameStart
-  | GameRound Int RoundStatus
+  | GameRound RoundInfo RoundStatus
   | GameDone
   deriving (Show, Eq)
+
+data RoundInfo = RoundInfo
+  { roundNum   :: Int
+  , categories :: [Category]
+  , letter     :: Char
+  } deriving (Show, Eq)
 
 data RoundStatus
   = WriteAnswers
@@ -61,7 +75,18 @@ initPlayer playerName game = game { players = Set.insert playerName (players gam
 getPlayers :: Game -> Set PlayerName
 getPlayers = players
 
+generateRound :: Int -> IO RoundInfo
+generateRound roundNum = do
+  categories <- take numCategories <$> shuffleM allCategories
+  letter <- getRandomR ('A', 'Z')
+  return RoundInfo{..}
+  where
+    numCategories = 12
+
 {- Scattergories logic -}
 
 type Category = Text
 type Answer = Text
+
+allCategories :: [Category]
+allCategories = Text.lines $(embedStringFile "../data/categories.txt")
