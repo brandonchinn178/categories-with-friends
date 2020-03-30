46b6c2dc2b85c5523ff0e968fcd3dc4dff158ee6
Brandon Chinn <brandon@leapyear.io>
2020-03-29 21:17:14 -0700

    Reformat Scattergories.Game

--- server/src/Scattergories/Game.hs
+++ server/src/Scattergories/Game.hs
@@ -9,24 +9,29 @@
 
 module Scattergories.Game
   ( Game
-  , GameStatus(..)
-  , SGameStatus(..)
-  , PlayerName
-  , createGame
-  , getStatus
-  , markGameDone
-  , getHost
-  , initPlayer
-  , getPlayers
-  , Category
-  , Answer
   , GameRound(GameRound, roundNum, categories, letter, endTime)
   , Vote(..)
-  , getAnswers
+  , createGame
+    -- * Players
+  , PlayerName
+  , getHost
+  , getPlayers
+  , initPlayer
+    -- * Game status
+  , GameStatus(..)
+  , SGameStatus(..)
+  , getStatus
+  , markGameDone
+    -- * Game round logistics + operations
   , isRoundDone
   , getCurrRound
   , updateCurrRound
   , startRound
+    -- * Categories
+  , Category
+    -- * Answering phase
+  , Answer
+  , getAnswers
   , addPlayerAnswers
   , haveAllPlayersAnswered
   ) where
@@ -42,57 +47,13 @@ import qualified Data.Text as Text
 import Data.Time (UTCTime, addUTCTime, getCurrentTime)
 import System.Random.Shuffle (shuffleM)
 
-{- Game infrastructure -}
-
-type PlayerName = Text
-
 data Game (status :: GameStatus) = Game
   { host    :: PlayerName
   , players :: Set PlayerName
+  , status  :: SGameStatus status
   , rounds  :: [GameRound]
     -- ^ Invariant: non-empty if status is not GameLoading
-  , status  :: SGameStatus status
   } deriving (Show)
-
-createGame :: PlayerName -> Game 'GameLoading
-createGame host = Game
-  { host
-  , players = Set.empty
-  , rounds = []
-  , status = SGameLoading
-  }
-
-getStatus :: Game status -> SGameStatus status
-getStatus = status
-
-markGameDone :: Game status -> Game 'GameDone
-markGameDone game = game { status = SGameDone }
-
-getHost :: Game status -> PlayerName
-getHost = host
-
-initPlayer :: PlayerName -> Game 'GameLoading -> Game 'GameLoading
-initPlayer playerName game = game { players = Set.insert playerName (players game) }
-
-getPlayers :: Game status -> [PlayerName]
-getPlayers = Set.toList . players
-
-{- Game status -}
-
-data GameStatus = GameLoading | GameInProgress | GameDone
-
--- | A witness that a Game is in one of the above statuses.
-data SGameStatus (status :: GameStatus) where
-  SGameLoading :: SGameStatus 'GameLoading
-  SGameInProgress :: SGameStatus 'GameInProgress
-  SGameDone :: SGameStatus 'GameDone
-
-deriving instance Show (SGameStatus status)
-
-{- Game rounds -}
-
-type Category = Text
-type Answer = Text
 
 data GameRound = GameRound
   { roundNum   :: Int
@@ -107,8 +68,46 @@ data GameRound = GameRound
 data Vote = NO_VOTE | INVALID | VALID
   deriving (Show, Eq)
 
-getAnswers :: GameRound -> Map PlayerName (Map Category Answer)
-getAnswers = fmap (fmap fst) . answers
+createGame :: PlayerName -> Game 'GameLoading
+createGame host = Game
+  { host
+  , players = Set.empty
+  , rounds = []
+  , status = SGameLoading
+  }
+
+{- Players -}
+
+type PlayerName = Text
+
+getHost :: Game status -> PlayerName
+getHost = host
+
+getPlayers :: Game status -> [PlayerName]
+getPlayers = Set.toList . players
+
+initPlayer :: PlayerName -> Game 'GameLoading -> Game 'GameLoading
+initPlayer playerName game = game { players = Set.insert playerName (players game) }
+
+{- Game status -}
+
+data GameStatus = GameLoading | GameInProgress | GameDone
+
+-- | A witness that a Game is in one of the above statuses.
+data SGameStatus (status :: GameStatus) where
+  SGameLoading :: SGameStatus 'GameLoading
+  SGameInProgress :: SGameStatus 'GameInProgress
+  SGameDone :: SGameStatus 'GameDone
+
+deriving instance Show (SGameStatus status)
+
+getStatus :: Game status -> SGameStatus status
+getStatus = status
+
+markGameDone :: Game status -> Game 'GameDone
+markGameDone game = game { status = SGameDone }
+
+{- Game round logistics + operations -}
 
 isRoundDone :: GameRound -> Bool
 isRoundDone = all (all isAnswerVoted) . answers
@@ -153,6 +152,20 @@ startRound game = do
     numCategories = 12
     roundDuration = 3 * 60 -- 3 minutes
 
+{- Categories -}
+
+type Category = Text
+
+allCategories :: [Category]
+allCategories = Text.lines $(embedStringFile "../data/categories.txt")
+
+{- Answering phase -}
+
+type Answer = Text
+
+getAnswers :: GameRound -> Map PlayerName (Map Category Answer)
+getAnswers = fmap (fmap fst) . answers
+
 addPlayerAnswers :: PlayerName -> Map Category Answer -> GameRound -> GameRound
 addPlayerAnswers playerName playerAnswers gameRound = gameRound
   { answers = Map.insert playerName (initVotes playerAnswers) (answers gameRound)
@@ -165,8 +178,3 @@ haveAllPlayersAnswered :: Game 'GameInProgress -> Bool
 haveAllPlayersAnswered game = all (`Map.member` answers) $ getPlayers game
   where
     GameRound{answers} = getCurrRound game
-
-{- Data -}
-
-allCategories :: [Category]
-allCategories = Text.lines $(embedStringFile "../data/categories.txt")
