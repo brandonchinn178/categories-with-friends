489e777cee622ba4fde616c5d34e6b02d696bd1a
Matthew Chinn <matthewchinn@Matthews-MacBook-Pro.local>
2020-03-29 18:11:37 -0700

    Removing all files in .gitignore

--- client/lib/src/root/root.dart
+++ client/lib/src/root/root.dart
@@ -1,9 +1,17 @@
 import 'package:angular/angular.dart';
+import 'package:angular_components/material_input/material_input.dart';
+import 'package:angular_components/material_button/material_button.dart';
 
 @Component(
   selector: 'root',
   templateUrl: 'root.html',
   styleUrls: ['root.css'],
-  directives: [],
+  directives: [materialInputDirectives, MaterialButtonComponent],
 )
-class RootComponent {}
+class RootComponent {
+  String gameId = '';
+
+  void onEnter() {
+    print('Route here');
+  }
+}
